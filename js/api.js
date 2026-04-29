8b744641952c50bdac95be7edb983b19e203f52d
Samuel Popoola <samuelpopoola323@gmail.com>
2026-04-29 09:25:04 +0100

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,42 @@
-n
+const api = {
+    request: async (endpoint, options = {}) => {
+        const token = localStorage.getItem("kb_token");
+
+        const headers = {
+            "Content-Type": "application/json",
+            ...(token && { Authorization: `Bearer ${token}` }),
+            ...options.headers
+        };
+
+        try {
+            const response = await fetch(`${CONFIG.API_BASE_URL}${endpoint}`, {
+                ...options,
+                headers
+            });
+
+            const data = await response.json();
+
+            if (!response.ok) {
+                throw new Error(data.message || "Something went wrong");
+            }
+
+            return data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    get: (endpoint) => api.request(endpoint, { method: "GET" }),
+
+    post: (endpoint, body) => api.request(endpoint, {
+        method: "POST",
+        body: JSON.stringify(body)
+    }),
+
+    put: (endpoint, body) => api.request(endpoint, {
+        method: "PUT",
+        body: JSON.stringify(body)
+    }),
+
+    delete: (endpoint) => api.request(endpoint, { method: "DELETE" })
+};
