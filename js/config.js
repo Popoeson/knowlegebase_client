da6cb3075e0d5232bf4dfb967f8fc6b5063f7233
Samuel Popoola <samuelpopoola323@gmail.com>
2026-04-29 09:28:01 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    API_BASE_URL: "https://your-render-url.onrender.com/api",
-    PAYSTACK_PUBLIC_KEY: "pk_test_xxxxxxxxxxxx"
+    API_BASE_URL: "https://knowlegebase-server.onrender.com/api",
+    PAYSTACK_PUBLIC_KEY: "pk_test_72e7641a30dcdbad32a51606354065cc5b62f20f"
 };
